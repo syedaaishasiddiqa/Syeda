0e649e4d5438a8b6d305a13192a2e1280ceb25b7
syedaaishasiddiqa <66374184+syedaaishasiddiqa@users.noreply.github.com>
2020-11-19 16:20:27 +0530

    Add files via upload

---
+++ OwnerDetails.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 13:49:59 2020
+
+@author: Hp
+"""
+import pandas as pd
+
+details = pd.Series([[{"Name":"Suresh","C.NO":"hjyt64882991z","Address":"H.No:12,2nd cross,RC road,Hassan","Ph no":"6677884455"}],
+                      [{"Name":"Mahesh","C.NO":"yeud64738274k","Address":"H.No:45,1st stage,BG Nagar,Hassan","Ph no":"9988447332"}],
+                      [{"Name":"Sujith","C.NO":"trjw63728364j","Address":"H.No:67,14th cross,,Banglore","Ph no":"9988446633"}],
+                      [{"Name":"Siddiq","C.NO":"qwdk648274992m","Address":"H.No:87,2nd cross,Near City Bus stand,Hassan","Ph no":"9522664455"}],
+                      [{"Name":"Aishwarya","C.NO":"dwde3127248b","Address":"H.No:101,2nd cross,Near Main Bus Stad,Hassan","Ph no":"8855221144"}],
+                      [{"Name":"Kalki","C.NO":"dwde3127248b","Address":"H.No:108,2nd cross,Near Main Bus Stad,Mangaluru","Ph no":"8852222255"}],
+                      [{"Name":"Kushal","Car No":"S7GT7","C.NO":"ddfe77886678b","Address":"H.No:108,2nd cross,Near Main Bus Stad Tumkur","Ph no":"8852222255"},
+                       {"Name":"Nagesh","Car No":"57GT1","C.NO":"drut6564738281","Address":"H.No:123,Near Golden Temple ,Coorgh","Ph no":"7788665554"}]
+                      ], index=['MCLRNF1', "AP28DU4396", "TS07FX3534","MA01AV8866","HR26D0555","6P609","S7GTT"])
+
+
+
+
+
+
+
